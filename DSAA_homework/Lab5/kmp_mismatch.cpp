ada71fefbd3ae634621bdb6fc606d9c62606a4f8
night-gale <2583942273@qq.com>
2019-10-16 21:07:26 +0800

    kmp mismatch function finished untested

---
+++ DSAA_homework/Lab5/kmp_mismatch.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+void mismatch(int *mmArray, char *_string,  int size) {
+    mmArray[0] = 0;
+    for(int i = 1; i < size; i++) {
+        int temp = mmArray[i - 1];
+        while(true) {
+            if(_string[temp] == _string[i]) {
+                mmArray[i] = temp + 1;
+                break;
+            }
+            
+            if(temp == 0) {
+                mmArray[i] = 0;
+                break;
+            }
+            temp = mmArray[temp];
+        }
+    }
+}
+
+int main(void) {
+    char _string[10000];
+    int mmArray[10000];
+    scanf("%s", &_string);
+    int length = -1;
+    while(_string[++length] != '\0');
+    mismatch(mmArray, _string, length);
+    for(int i = 0; i < length; i++) {
+        printf("%d ", mmArray[i]);
+    }
+}
