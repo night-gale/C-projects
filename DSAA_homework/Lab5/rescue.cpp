cb62622cdfe4eb529359271ad8e1a5ffead7c14b
night-gale <2583942273@qq.com>
2019-10-17 21:27:19 +0800

    rescue to be debugged

---
+++ DSAA_homework/Lab5/rescue.cpp
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#define SIZE 50
+void mismatch(int *, char *, int);
+char key[256];
+char code[SIZE];
+char decoded[SIZE];
+int mmArray[SIZE];
+
+int main(void) {
+    char temp[2];
+    int size = -1;
+    int cptr = 0;
+    int dptr = 0;
+    for(int i = 0; i < 26; i++) {
+        scanf("%s", &temp);
+        key['a' + i] = temp[0];
+    }
+    scanf("%s", &code);
+    while(code[++size] != '\0');
+    for(int i = 0; i < size; i++) {
+        decoded[i] = key[code[i]];
+    }
+    mismatch(mmArray, code, size);
+
+    dptr = size / 2 - 1;
+    while(dptr != size ) {
+        if(code[cptr] == decoded[dptr]) {
+            cptr++;
+            dptr++;
+        }else {
+            if(cptr != 0) {
+                cptr = mmArray[cptr - 1];
+            }else dptr++;
+        }
+    }
+    printf("%d\n", size - cptr);
+}
+
+void mismatch(int *mmArray, char *_string,  int size) {
+    mmArray[0] = 0;
+    for(int i = 1; i < size; i++) {
+        int temp = mmArray[i - 1];
+        while(true) {
+            if(_string[temp] == _string[i]) {
+                mmArray[i] = temp + 1;
+                break;
+            }
+            
+            if(temp == 0) {
+                mmArray[i] = 0;
+                break;
+            }
+            temp = mmArray[temp - 1];
+        }
+    }
+}
