51942abedf26a0d2729db3df88ae1d1995faf191
night-gale <2583942273@qq.com>
2019-11-06 18:27:39 +0800

    priority tree tested...

---
+++ DSAA_homework/Lab7/cut_the_stick_pro.cpp
@@ -0,0 +1,155 @@
+#include <stdio.h>
+
+class Node {
+    public:
+    long long data;
+    Node* left;
+    Node* right;
+    Node* parent;
+
+    Node() {
+        Node(0);
+    }
+    Node(long long data) {
+        this->data = data;
+        left = 0;
+        right = 0;
+        parent = 0;
+    }
+};
+
+class P_Queue {
+    public:
+    Node* root;
+    int size;
+    int stack[100];
+    int top;
+    P_Queue() {
+        root = 0;
+        size = 0;
+        top = -1;
+    }
+
+    void insert(long long data) {
+        Node* newNode = new Node(data);
+        size++;
+        if(!root) {
+            root = newNode;
+            return;
+        }else {
+            Node* current = find(size);
+            if(stack[top--]) {
+                current->right = newNode;
+            }else {
+                current->left = newNode;
+            }
+            newNode->parent = current;
+            current = newNode;
+            heapfy(current);
+        }
+    }
+
+    long long delete_MIN() {
+        if(size == 1) {
+            long long _data = root->data;
+            delete root;
+            root = NULL;
+            size = 0;
+            return _data;
+        }
+        Node* current = find(size);
+        current = stack[top]? current->right: current->left;
+        swap(current, root);
+        size--;
+        Node* temp = root;
+        Node* next;
+        long long _data = current->data;
+        current = current->parent;
+        
+        if(stack[top--]) {
+            delete current->right;
+            current->right = NULL;
+        }else {
+            delete current->left;
+            current->left = NULL;
+        }
+        while(temp) {
+            if(temp->left && temp->right) {
+                next = temp->left->data > temp->right->data? temp->right: temp->left;
+            }else if(temp->left) {
+                next = temp->left;
+            }else if(temp->right) {
+                next = temp->right;
+            }else {
+                next = NULL;
+            }
+            if(next && next->data < temp->data) {
+                swap(next, temp);
+                temp = next;
+            }else {
+                break;
+            }
+        }
+
+        return _data;
+    }
+
+    private:
+    Node* find(int _size) {
+        int temp = _size;
+        Node* current = root;
+        while(temp != 1) {
+            stack[++top] = temp & 1;
+            temp = temp >> 1;
+        }
+        while(top >= 1) {
+            if(stack[top--]) {
+                current = current->right;
+            }else {
+                current = current->left;
+            }
+        }
+        return current;
+    }
+
+    void swap(Node* a, Node* b) {
+        long long temp = a->data;
+        a->data = b->data;
+        b->data = temp;
+    }
+
+    void heapfy(Node* current) {
+        while(current->parent && current->data < current->parent->data) {
+            swap(current, current->parent);
+            current = current->parent;
+        }
+    }
+
+};
+
+int T, N;
+
+int main(void) {
+    scanf("%d", &T);
+
+    for(int i = 0; i < T; i++) {
+        P_Queue queue;
+        scanf("%d", &N);
+        long long temp;
+        long long cost = 0;
+        // int pend = N;
+
+        for(int j = 0; j < N; j++) {
+            scanf("%lld", &temp);
+            queue.insert(temp);
+        }
+        for(int k = 0; k < N - 1; k++) {
+            long long sma = queue.delete_MIN();
+            long long se_sma = queue.delete_MIN();
+            
+            queue.insert(sma + se_sma);
+            cost += sma + se_sma;
+        }
+        printf("%lld\n", cost);
+    }
+}
--- DSAA_homework/Lab7/priority_queue.cpp
+++ DSAA_homework/Lab7/priority_queue.cpp
@@ -5,14 +5,16 @@ class Node {
     long long data;
     Node* left;
     Node* right;
+    Node* parent;
 
     Node() {
         Node(0);
     }
-    Node(int data) {
+    Node(long long data) {
         this->data = data;
         left = 0;
         right = 0;
+        parent = 0;
     }
 };
 
@@ -35,33 +37,110 @@ class P_Queue {
             root = newNode;
             return;
         }else {
-            int temp = size;
-            Node* current = root;
-            while(temp != 1) {
-                stack[++top] = temp & 1;
-                temp = temp >> 1;
-            }
-            while(top != 0) {
-                if(stack[top--]) {
-                    current = current->right;
-                }else {
-                    current = current->left;
-                }
-            }
+            Node* current = find(size);
             if(stack[top--]) {
                 current->right = newNode;
             }else {
                 current->left = newNode;
             }
+            newNode->parent = current;
+            current = newNode;
+            heapfy(current);
         }
     }
+
+    long long delete_MIN() {
+        if(size == 1) {
+            long long _data = root->data;
+            delete root;
+            root = NULL;
+            size = 0;
+            return _data;
+        }
+        Node* current = find(size);
+        current = stack[top]? current->right: current->left;
+        swap(current, root);
+        size--;
+        Node* temp = root;
+        Node* next;
+        long long _data = current->data;
+        current = current->parent;
+        
+        if(stack[top--]) {
+            delete current->right;
+            current->right = NULL;
+        }else {
+            delete current->left;
+            current->left = NULL;
+        }
+        while(temp) {
+            if(temp->left && temp->right) {
+                next = temp->left->data > temp->right->data? temp->right: temp->left;
+            }else if(temp->left) {
+                next = temp->left;
+            }else if(temp->right) {
+                next = temp->right;
+            }else {
+                next = NULL;
+            }
+            if(next && next->data < temp->data) {
+                swap(next, temp);
+                temp = next;
+            }else {
+                break;
+            }
+        }
+
+        return _data;
+    }
+
+    private:
+    Node* find(int _size) {
+        int temp = _size;
+        Node* current = root;
+        while(temp != 1) {
+            stack[++top] = temp & 1;
+            temp = temp >> 1;
+        }
+        while(top >= 1) {
+            if(stack[top--]) {
+                current = current->right;
+            }else {
+                current = current->left;
+            }
+        }
+        return current;
+    }
+
+    void swap(Node* a, Node* b) {
+        long long temp = a->data;
+        a->data = b->data;
+        b->data = temp;
+    }
+
+    void heapfy(Node* current) {
+        while(current->parent && current->data < current->parent->data) {
+            swap(current, current->parent);
+            current = current->parent;
+        }
+    }
+
 };
 
+void switchPointer(int* n) {
+    n = NULL;
+}
 
 int main(void) {
     P_Queue a;
-    a.insert(1);
-    a.insert(2);
-    a.insert(3);
-    a;
+    int n;
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++) {
+        long long temp;
+        scanf("%lld", &temp);
+        a.insert(temp);
+    }
+    for(int i = 0; i < n; i++) {
+        printf("%lld ", a.delete_MIN());
+    }
 }
