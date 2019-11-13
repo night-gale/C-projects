500e0a03e21d19650fece793de7472693203f8bc
night-gale <2583942273@qq.com>
2019-11-14 01:29:32 +0800

    delete completed not tested yet insert tested

--- DSAA_homework/Lab7/balanced_tree.cpp
+++ DSAA_homework/Lab7/balanced_tree.cpp
@@ -40,7 +40,7 @@ class AVLtree {
         if(!root) {
             root = new Node(data, 0);
         }else {
-            Node* current = find(data);
+            Node* current = find(data, root);
             if(!current) {
                 Node* child = new Node(data, 0);
                 Node* par = stack[top--];
@@ -61,14 +61,14 @@ class AVLtree {
                         if(status == -1) {
                             //RR
                             LRotate(par);
-                        }else if(status == 1) {
+                        }else if(status == 1 || status == 0) {
                             //RL
                             RRotate(par->right);
                             LRotate(par);
                         }
                     }else if(status == 2) {
                         status = isBalanced(par->left);
-                        if(status == -1) {
+                        if(status == -1 || status == 0) {
                             //LR
                             LRotate(par->left);
                             RRotate(par);
@@ -82,7 +82,112 @@ class AVLtree {
         }
     }
 
-    Node* find(long long key) {
+    void delete_Node(long long key) {
+        Node* current = find(key, root);
+        Node* par = current->parent;
+        if(!current->left && !current->right) {
+            //leaf node
+            if(!par) {
+                delete root;
+                root = 0;
+            }else {
+                if(par->left == current) {
+                    delete par->left;
+                    par->left = 0;
+                }else {
+                    delete par->right;
+                    par->right = 0;
+                }
+            }
+        }else {
+            //eternal node
+            Node* relp;;
+            if(current->left) {
+                //has left node
+                relp = current->left;
+                while(relp->right) {
+                    stack[++top] = relp;
+                    relp = relp->right;
+                }
+                if(relp != current->left) {
+                    //current node has a predecessor
+                    current->data = relp->data;
+                    relp->parent->right = relp->left;
+                    if(relp->left) relp->left->parent = relp->parent->left;
+                    getDepth(current->left);
+                    getDepth(current);
+                    if(par) getDepth(par);
+                    delete relp;
+                }else {
+                    //current node does not have a predecessor
+                    current->data = relp->data;
+                    if(relp->left) relp->left->parent = current;
+                    current->left = relp->left;
+                    if(current->left) getDepth(current->left);
+                    delete relp;
+                    getDepth(current);
+                    if(par) getDepth(par);
+                }
+            }else {
+                relp = current->right;
+                while(relp->left) {
+                    stack[++top] =relp;
+                    relp = relp->right;
+                }
+                if(relp != current->right) {
+                    //current node has a successor
+                    current->data = relp->data;
+                    relp->parent->left = relp->right;
+                    if(relp->right) relp->right->parent = relp->parent->right;
+                    getDepth(current->right);
+                    getDepth(current);
+                    if(par) getDepth(par);
+                    delete relp;
+                }else {
+                    //current node does not have a successor
+                    current->data = relp->data;
+                    if(relp->right) relp->right->parent = current;
+                    current->right = relp->right;
+                    if(current->right) getDepth(current->right);
+                    delete relp;
+                    getDepth(current);
+                    if(par) getDepth(par);
+                }
+            }
+            
+            while(top != -1) {
+                current = stack[top--];
+                int status = isBalanced(current);
+                if(status == -2) {
+                    //right > left
+                    status = isBalanced(current->right);
+                    if(status == -1) {
+                        //RR inbalance
+                        LRotate(current);
+                    }else if(status == 1 || status == 0) {
+                        //RL inbalance
+                        RRotate(current->right);
+                        LRotate(current);
+                    }else {
+
+                    }
+                }else if(status == 2) {
+                    //left > right
+                    status = isBalanced(current->left);
+                    if(status == 1) {
+                        //LL inbalance
+                        RRotate(current);
+                    }else if(status == -1 || status == 0) {
+                        //LR inbalance
+                        LRotate(current->left);
+                        RRotate(current);
+                    }
+                }
+            }
+        }
+    }
+
+    Node* find(long long key, Node* subroot) {
         Node* current = root;
         while(current) {
             stack[++top] = current;
@@ -121,13 +226,14 @@ class AVLtree {
             }else {
                 subroot->parent->right = right;
             }
-            getDepth(subroot->parent);
         }else {
             this->root = right;
+            right->parent = 0;
         }
-
+        subroot->parent = right;
         getDepth(subroot);
         getDepth(right);
+        if(right->parent) getDepth(right->parent);
     }
 
     void getDepth(Node* subroot) {
@@ -154,19 +260,22 @@ class AVLtree {
             }else {
                 subroot->parent->right = left;
             }
-            getDepth(subroot->parent);
+            
         }else {
             this->root = left;
+            left->parent = 0;
         }
+        subroot->parent = left;
         getDepth(subroot);
         getDepth(left);
+        if(left->parent) getDepth(left->parent);
     }
 
     void print_tree(Node* subroot) {
         if(subroot->left) {
             print_tree(subroot->left);
         }
-        printf("%d ", subroot->data);
+        printf("%lld ", subroot->data);
         if(subroot->right) {
             print_tree(subroot->right);
         }
@@ -177,10 +286,9 @@ int main(void) {
     int n;
     scanf("%d", &n);
     AVLtree atree;
-
+    long long data;
     for(int i = 0; i < n; i++) {
-        long long data;
-        scanf("%d", &data);
+        scanf("%lld", &data);
         atree.insert(data);
     }
     atree.print_tree(atree.root);
