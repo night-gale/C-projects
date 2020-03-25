3756c755bcb3d88b138e30ed45bec3c83567d0b9
night-gale <2583942273@qq.com>
2020-03-25 23:36:15 +0800

    failed!

--- ADA/Lab4/yu-gi-oh.cpp
+++ ADA/Lab4/yu-gi-oh.cpp
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <unordered_map>
 #include <chrono>
+#include <algorithm>
 
 const int SIZE = 11;
 const int A_MAX = 100;
-int results[5][2] = {{3,0},{2,1},{1,1},{1,2},{0,3}};
+int results[5][2] = {{3,0},{2,1},{1,2},{0,3},{1,1}};
 int N;
 int scores[SIZE];
-long long DFS(int x, int y, int* scores);
+long long DFS(int x, int y, int th, int tw);
 const long long modulo = 998244353;
 std::unordered_map<long long, long long> map;
-long long hash(int x, int y);
+long long hash(int x);
 int th,tw;
 
 int main(void) {
@@ -25,7 +26,7 @@ int main(void) {
     th = tsum - N*(N-1);
     tw = N*(N-1)/2*3-tsum;
 
-    long long result = DFS(0, 0, scores);
+    long long result = DFS(0, 0, 0, 0);
 
     printf("%lld\n", result);
     auto end = std::chrono::high_resolution_clock::now();
@@ -33,37 +34,52 @@ int main(void) {
     printf("%f", elapsed_time.count());
 }
 
-long long DFS(int x, int y, int* scores) {
+long long DFS(int x, int y, int tht, int twt) {
     long long cnt = 0;
     long long sum = 0;
     long long h1;
 
-    if(x == y) {
-        for(int i = x; i < N; i++) {
-            if(scores[i] > 3 * (N-x-1)) {
-                return 0;
-            }
+
+    // for(int i = x; i < N; i++) {
+    //     sum += scores[i];
+    // }
+
+    // if(sum > ((N-x)*(N-x-1)/2 - (y-x)) * 3 || sum < ((N-x)*(N-x-1)/2 - (y-x))*2) {
+    //     return 0;
+    // }
+
+    // if(tht == th) {
+    //     if(scores[x] != (N-y-1)) return 0;
+    //     for(int i = x + 1; i < N; i++) {
+    //         if(i <= y) {
+    //             if(scores[i] != (N-x-2)) return 0;
+    //         }else {
+    //             if(scores[i] != (N-x-1)) return 0;
+    //         }
+    //     }
+    //     return 1;
+    // }
+    
+    if(x==y) {
+        h1 = hash(x);
+        if(map.find(h1) != map.end()) {
+            return map[h1];
         }
     }
-
-    for(int i = x; i < N; i++) {
-        sum += scores[i];
-    }
-
-    if(sum > ((N-x)*(N-x-1)/2 - (y-x)) * 3 || sum < ((N-x)*(N-x-1)/2 - (y-x))*2) {
-        return 0;
-    }
-    
-    h1 = hash(x, y);
-    if(map.find(h1) != map.end()) {
-        return map[h1];
-    }
+    // if(x == y) {
+    //     for(int i = x; i < N; i++) {
+    //         if(scores[i] > 3 * (N-x-1)) {
+    //             map[h1] = 0;
+    //             return 0;
+    //         }
+    //     }
+    // }
 
     if(y >= N - 1 && x < N - 1) {
         if(scores[x] != 0) {
             return 0;
         }else {
-            return DFS(x + 1, x + 1, scores);
+            return DFS(x + 1, x + 1, tht, twt);
         }
     }else if(x >= N - 1) {
         if(scores[N-1] != 0) {
@@ -74,27 +90,43 @@ long long DFS(int x, int y, int* scores) {
     }
     
     for(int i = 0; i < 5; i++) {
+        if(tht >= th && i < 4) continue;
         scores[x] -= results[i][0];
         scores[y + 1] -= results[i][1];
-        if(scores[x] >= 0 && scores[y + 1] >= 0) {
-            cnt = (cnt % modulo + DFS(x, y + 1, scores) % modulo) % modulo;
+        if(scores[x] >= 0 && scores[x] <= 3*(N-y-2) && scores[y + 1] >= 0) {
+            if(i < 4 ) {
+                if(tht < th)
+                cnt = (cnt % modulo + DFS(x, y + 1, tht+1, twt) % modulo) % modulo;
+            }else {
+                if(twt < tw)
+                cnt = (cnt % modulo + DFS(x, y + 1, tht, twt+1) % modulo) % modulo;
+            }
         }
         scores[x] += results[i][0];
         scores[y + 1] += results[i][1];
     }
 
-    // if(x == y) {
-        map[hash(x,y)] = cnt;
-    // }
+    if(x == y) {
+        map[h1] = cnt;
+    }
     return cnt;
 }
 
-long long hash(int x, int y) {
+
+long long hash(int x) {
     long long sum = 0;
-    for(int i = 0; i < N; i++) {
-        sum = sum * 31 + scores[i];
+    int* tscores = new int[N];
+    for(int i = x; i < N; i++) {
+        tscores[i-x] = scores[i];
     }
-    sum = sum * 10 + x;
-    sum = sum * 10 + y;
+
+    std::sort(tscores, tscores+N-x);
+
+    for(int i = 0; i < N-x; i++) {
+        sum = sum * 31 + tscores[i];
+    }
+    // sum = sum * 10 + x;
+    // sum = sum * 10 + y;
+    delete [] tscores;
     return sum;
 }
